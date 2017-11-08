9f96581bbbe8e695b6a94db6152ef78f766f1033
Jason Figueroa <jason.figueroa2@gmail.com>
2017-11-08 15:03:58 -0600

    added module to check if email exists in database

---
+++ scripts/userEmailInDb.js
@@ -0,0 +1,23 @@
+// Jason Figueroa
+// Module checks to see if a email exists in database
+
+// Module accepts a email and an object
+// Searches the object for key "users" 
+// Then iterates the value of key, an array of objects 
+// Then searches each object in array for key "email"
+// If value of key "email" in object matches parameter email function returns true 
+
+function userNameInDb(email, dbObj) {
+
+    let emailInDb = false
+
+    dbObj.users.forEach(userObj => {
+        if(userObj.email === email) {
+            emailInDb = true
+        }
+    })
+
+    return emailInDb
+}
+
+module.exports = userNameInDb
