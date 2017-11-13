835dcad8791d845df082b655e18863cb3be4f8d3
Tyler <bowmant388@gmail.com>
2017-11-13 11:03:53 -0600

    added friendObjFatory.js

---
+++ scripts/friendObjFactory.js
@@ -0,0 +1,30 @@
+const getLocalStorage = require("./getLocalStorage")
+const idGenerator = require("./idGenerator")
+const getSessionStorage = require("./getSessionStorage")
+
+const mainDB = getLocalStorage()
+let activeUser = getSessionStorage()
+
+
+let friendObjFactory = (friendObj) => {
+    let lastId = activeUser.user.friends[activeUser.user.friends.length-1] || {"friendId":0}
+    let friendIdGenerator = idGenerator(lastId.friendId)
+    mainDB.users.friends.push(Object.create(null, {
+        "friendId": {
+            "value": friendIdGenerator.next().value,
+            "enumerable": true
+        },
+        "friendUserId": {
+            "value": friendObj.userId,
+            "enumerable": true
+        },
+        "friendFirstName": {
+            "value": friendObj.firstName,
+            "enumerable": true
+        },
+        "friendLastName": {
+            "value": friendObj.lastName,
+            "enumerable": true
+        }
+    }))
+}
