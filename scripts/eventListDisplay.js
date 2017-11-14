d63401208137c9ccb0c7553caaf0a7bf1213a3db
Kristen Norris <krnorris65@gmail.com>
2017-11-13 18:48:28 -0600

    added comments to eventListDisplay

--- scripts/eventListDisplay.js
+++ scripts/eventListDisplay.js
@@ -13,21 +13,18 @@ const displayEventList = function () {
     const storedDb = getLocalStorage()
     const storedEvents = storedDb.events //events in local storage
     
-    const eventListEl = document.getElementById("event_list")
-    
     //if there are events in local storage
     if (storedEvents.length > 0) {
         
         const currentUserId = getSessionStorage().user.userId //id of current user
+        let eventsToDisplay = [] //array that holds events that should be displayed in Upcoming events
         
         let friendsArray = storedDb.userFriend.filter(friend => {return currentUserId === friend.activeUserId || currentUserId === friend.friendUserId}) //array of current user's friends
-
-        let eventsToDisplay = [] //array that holds events that the current user's friends have created
 
         //filters through friendsArray and adds their events to the eventsToDisplay
         friendsArray.forEach(friend => {
             
-            let array = storedEvents.filter(cEvent => {
+            let friendEvents = storedEvents.filter(cEvent => {
                 const notActiveUser = (cEvent.userId !== currentUserId) //event userId is not equal to the session active user
                 const activeId = (cEvent.userId === friend.activeUserId) //event userId is equal to the active user on the friend relationship
                 const friendId = (cEvent.userId === friend.friendUserId) //event userId is equal to the friend user on the friend relationship
@@ -35,11 +32,8 @@ const displayEventList = function () {
                 return (notActiveUser && (activeId || friendId))//id is NOT equal to the session active user but is equal to either the active or friend user on the friend relationship
             })
 
-            let friendsEvents = eventsToDisplay.concat(array) //concat events into an array
+            eventsToDisplay = eventsToDisplay.concat(friendEvents) //adds friendEvents into eventsToDisplay 
 
-            eventsToDisplay = friendsEvents //adds friendEvents into eventsToDisplay
-
-            // console.log(friendsEvents)
         })
 
         //returns the events that have the current active user as the event userId
@@ -67,25 +61,3 @@ const displayEventList = function () {
 }
 
 module.exports = displayEventList
-
-// doesn't work yet:
-// const dateToday = Date.now()
-// const eventDate = Date.parse(event.eventDate)
-// if(eventDate > dateToday) { //doesn't display events that happened before today's date
-//     addEventList(event)
-// }
-
-// storedEvents.sort( function(a, b) { //sort events by date, need to parse into an integer
-//     const dateA = Date.parse(a.eventDate) 
-//     const dateB = Date.parse(b.eventDate)
-//     return dateA - dateB
-// }).forEach( event => {
-//     if(event.userId === currentUserId) { //if the event userId matches the id of the current user 
-//         const dateToday = Date.parse(todayDate()) //today's date parsed
-//         const eventDate = Date.parse(event.eventDate) //event date parsed
-//         if(dateToday <= eventDate) {
-//             addEventList(event) //then add the event to the DOM
-//             document.getElementById("event_None").className = "hideIt" //and give the default message a class of hideIt
-//         }
-//     }   
-// })
