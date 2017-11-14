79cd206e84190b293477bcc93009b715b2f30c80
Kristen Norris <krnorris65@gmail.com>
2017-11-14 10:31:40 -0600

    debugging friend page

--- scripts/displayUsers.js
+++ scripts/displayUsers.js
@@ -2,25 +2,33 @@ const getLocalStorage = require("./getLocalStorage")
 const getSessinStorage = require("./getSessionStorage")
 const addFriend = require("./addFriend")
 
-const mainDB = getLocalStorage()
 
 const displayUsers = () => {
+    const mainDB = getLocalStorage()
     const activeUser = getSessinStorage()
     let usersOutputEl = document.getElementById("Dashboard")
     usersOutputEl.innerHTML = ""
 
     let userString = ""
 
+    let userNot = []
+
+    let friendsArray = mainDB.userFriend.filter(friend => {return activeUser.user.userId === friend.activeUserId || activeUser.user.userId === friend.friendUserId}) //array of current user's friends
+
+        
+    console.log(friendsArray)
+    console.log(mainDB.users)
+
     mainDB.users.forEach(DbUser => {
-        if(DbUser.userId !== activeUser.user.userId && activeUser.user.friends.includes(DbUser)===false) {
-            userString+= 
+        // const notActiveUser = (DbUser.userId !== activeUser.user.userId) //userId is not equal to the session active user
+        userString+=
             `
             <section class= "user" id= "${DbUser.userId}">
                 <h2>${DbUser.firstName} ${DbUser.lastName}</h2>
                 <button class= "addButton" id="${DbUser.userId}">Add Friend</button>
             </section>
             `
-        }
+    
     })
 
     usersOutputEl.innerHTML = userString
