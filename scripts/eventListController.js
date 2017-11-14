8808b9ede7b76fbb19e9d1fe2074b42dc96b287c
Kristen Norris <krnorris65@gmail.com>
2017-11-14 11:57:39 -0600

    added class to friends events

--- scripts/eventListController.js
+++ scripts/eventListController.js
@@ -10,24 +10,27 @@ const addEventList = function (event) {
 
     const eventEl = document.createElement("section")
     eventEl.id = `event_${event.eventId}`
-
+    
     let eventContentString = `
-        <h3 id="event_name${event.eventId}">${event.eventName}</h3>
-        <p id="event_date${event.eventId}">Date: ${event.eventDate}</p>
-        <p id="event_location${event.eventId}">Location: ${event.eventLocation}</p>
-        <p id="event_description${event.eventId}">Description: ${event.eventDescription}</p>
+    <h3 id="event_name${event.eventId}">${event.eventName}</h3>
+    <p id="event_date${event.eventId}">Date: ${event.eventDate}</p>
+    <p id="event_location${event.eventId}">Location: ${event.eventLocation}</p>
+    <p id="event_description${event.eventId}">Description: ${event.eventDescription}</p>
     `
-
+    
     eventEl.innerHTML += eventContentString
-
+    
     //only allow user to edit the event that they created
     if(event.userId === activeUser.userId) {
+        
         const editEventButton = document.createElement("button")
         editEventButton.className = `edit_${event.eventId}`
         editEventButton.appendChild(document.createTextNode("Edit Event"))
         editEventButton.addEventListener("click", editEvent)
 
         eventEl.appendChild(editEventButton)
+    } else {
+        eventEl.className = "friendEvent" //added class to identify events that are for the user's friends
     }
 
     eventListEl.appendChild(eventEl)
