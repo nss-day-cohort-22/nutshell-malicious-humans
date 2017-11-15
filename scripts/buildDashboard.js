7f4ef5108d727ac60f7b3006f939047be69c88ee
Jason Figueroa <jason.figueroa2@gmail.com>
2017-11-14 19:00:25 -0600

    a more finalized styling for the dashboard

--- scripts/buildDashboard.js
+++ scripts/buildDashboard.js
@@ -28,11 +28,11 @@ const buildDashboard = function () {
     let tasksDiv = document.createElement("div")
     // tasksDiv.appendChild(document.createTextNode("Click to create or view Tasks"))
     tasksDiv.innerHTML = `
-    <i class="fa fa-tasks" aria-hidden="true"></i> Click to create or view Tasks<br>`
+    <i class="fa fa-tasks fa-2x" aria-hidden="true"></i> Click to create or view Tasks<br>`
     //create a button that allows users to navigate to the tasks page, give that button an id
     let tasksButton = document.createElement("button")
     tasksButton.classList.add("btn", "btn-primary")
-    tasksButton.appendChild(document.createTextNode("Click"))
+    tasksButton.appendChild(document.createTextNode("Tasks"))
     tasksButton.id = "tasks_Button"
     tasksButton.addEventListener("click", function(){
         taskController()
@@ -45,11 +45,11 @@ const buildDashboard = function () {
     let messagesDiv = document.createElement("div")
     // messagesDiv.appendChild(document.createTextNode("Click to create or view messages"))
     messagesDiv.innerHTML = `
-    <i class="fa fa-comments" aria-hidden="true"></i> Click to create or view messages<br>`
+    <i class="fa fa-comments fa-2x" aria-hidden="true"></i> Click to create or view messages<br>`
     //create a button that allows users to navigate to the tasks page, give that button an id
     let messagesButton = document.createElement("button")
     messagesButton.classList.add("btn", "btn-primary")
-    messagesButton.appendChild(document.createTextNode("Click"))
+    messagesButton.appendChild(document.createTextNode("Messages"))
     messagesButton.id = "messages_Button"
     messagesButton.addEventListener("click", buildMessageBoard)
     //append the button to the tasks div
@@ -60,11 +60,11 @@ const buildDashboard = function () {
     let eventsDiv = document.createElement("div")
     // eventsDiv.appendChild(document.createTextNode("Click to create or view events"))
     eventsDiv.innerHTML = `
-    <i class="fa fa-calendar" aria-hidden="true"></i> Click to create or view events<br>`
+    <i class="fa fa-calendar fa-2x" aria-hidden="true"></i> Click to create or view events<br>`
     //create a button that allows users to navigate to the tasks page, give that button an id
     let eventsButton = document.createElement("button")
     eventsButton.classList.add("btn", "btn-primary")
-    eventsButton.appendChild(document.createTextNode("Click"))
+    eventsButton.appendChild(document.createTextNode("Events"))
     eventsButton.id = "events_Button"
     eventsButton.addEventListener("click", eventController)
     //append the button to the tasks div
@@ -75,18 +75,18 @@ const buildDashboard = function () {
     let newsDiv = document.createElement("div")
     // newsDiv.appendChild(document.createTextNode("Click to create or view News"))
     newsDiv.innerHTML = `
-    <i class="fa fa-newspaper-o" aria-hidden="true"></i> Click to create or view News<br>`
+    <i class="fa fa-newspaper-o fa-2x" aria-hidden="true"></i> Click to create or view News<br>`
     //create a button that allows users to navigate to the tasks page, give that button an id
     let createNewsButton = document.createElement("button")
     createNewsButton.classList.add("btn", "btn-primary")
-    createNewsButton.appendChild(document.createTextNode("Post a News Article"))
+    createNewsButton.appendChild(document.createTextNode("Post Article"))
     createNewsButton.id = "create_news_button"
     createNewsButton.addEventListener("click", newsForm)
     //append the button to the tasks div
     newsDiv.appendChild(createNewsButton)
     let showNewsButton = document.createElement("button")
     showNewsButton.classList.add("btn", "btn-primary")
-    showNewsButton.appendChild(document.createTextNode("Show News Articles"))
+    showNewsButton.appendChild(document.createTextNode("Show Articles"))
     showNewsButton.id = "show_news_button"
     showNewsButton.addEventListener("click", writeNews)
     newsDiv.appendChild(showNewsButton)
@@ -96,7 +96,7 @@ const buildDashboard = function () {
     let friendsDiv = document.createElement("div")
     // friendsDiv.appendChild(document.createTextNode("Click to add or view Friends"))
     friendsDiv.innerHTML = `
-    <i class="fa fa-users" aria-hidden="true"></i> Click to add or view Friends<br>`
+    <i class="fa fa-users fa-2x" aria-hidden="true"></i> Click to add or view Friends<br>`
     friendsDiv.id = "friends"
     //create a button that allows users to navigate to the tasks page, give that button an id
     let friendsButton = document.createElement("button")
@@ -108,7 +108,7 @@ const buildDashboard = function () {
     friendsDiv.appendChild(friendsButton)
     let showFriendsButton = document.createElement("button")
     showFriendsButton.classList.add("btn", "btn-primary")
-    showFriendsButton.appendChild(document.createTextNode("Show Friends List"))
+    showFriendsButton.appendChild(document.createTextNode("Show Friends"))
     showFriendsButton.id = "show_Friends_button"
     showFriendsButton.addEventListener("click", displayFriends)
     friendsDiv.appendChild(showFriendsButton)
@@ -162,7 +162,7 @@ const buildDashboard = function () {
     colDiv2.classList.add("col")
     colDiv3.classList.add("col")
     colDiv4.classList.add("col")
-    colDiv5.classList.add("col")
+    colDiv5.classList.add("col-6")
 
     rowDiv1.appendChild(colDiv1)
     rowDiv1.appendChild(colDiv2)
